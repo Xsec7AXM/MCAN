a9eae1ed33f08124a427234dc7c347361dcbc2b3
AXM <44923880+Xsec7AXM@users.noreply.github.com>
2019-06-26 21:53:14 +0700

    Add files via upload

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+os.system('pkg install python -y && pkg install python2 -y')
+os.system('pkg install ruby -y && pkg install mpv -y')
+os.system('pkg install php -y && pkg install sl -y')
+os.system('gem install lolcat')
+os.system('pip install -r requirements.txt')
+os.system('pip2 install -r requirements.txt')
+os.system('cd /sdcard && mkdir Bakwan')
+os.system('chmod 777 mcan.py')
